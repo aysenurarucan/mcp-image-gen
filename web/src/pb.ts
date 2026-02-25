fdbcfb03cf77749ead5200aa38b5e4b31ca568a5
Ayşe Nur Arucan <137176580+aysenurarucan@users.noreply.github.com>
2026-02-25 10:28:00 +0300

    Update pb.ts

--- web/src/pb.ts
+++ web/src/pb.ts
@@ -1,6 +1,4 @@
 import PocketBase from "pocketbase";
 import { Capacitor } from "@capacitor/core";
 
-const HOST = Capacitor.isNativePlatform() ? "10.0.2.2" : "127.0.0.1";
-
-export const pb = new PocketBase(`http://${HOST}:8090`);
+export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
